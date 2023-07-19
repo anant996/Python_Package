8b920960dc61d2d762da1a63f4e5eafc320c7e1d
Anant Chaudhary <139758290+anant996@users.noreply.github.com>
2023-07-19 10:40:56 +0530

    original passwd.py

--- pkgAnant/passwd.py
+++ pkgAnant/passwd.py
@@ -1,41 +1,27 @@
 import random
 import string
-import csv
 
-def exclude_csv(path):
-    with open(path, newline='') as csvfile:
-        reader=csv.reader(csvfile)
-        return set(row[0] for row in reader)
+def generate_pass():
 
-def generate_pass(exclusions):
-    len_password = random.randint(6, 12)
+    # using random for range
+    len=random.randint(6, 12)
 
+    # using string to generate characters
     lower=string.ascii_lowercase
     upper=string.ascii_uppercase
     num=string.digits
 
-    password=random.choice(lower) + random.choice(upper) + random.choice(num)
-    password+=''.join(random.choices(lower + upper + num, k=len_password - 3))
-    password=''.join(char for char in password if char not in exclusions)
+    # to select at least one lower, one upper, one number
+    password=random.choice(lower)+random.choice(upper)+random.choice(num)
 
+    # filling password with remaining length
+    password+=''.join(random.choices(lower+upper+num,k=len-3))
+
+    # shuffling the password by creating a list
     password_list=list(password)
     random.shuffle(password_list)
     password=''.join(password_list)
     return password
 
-#calling exclude_csv function to collect all names from names.csv
-excluded_names=exclude_csv("names.csv")
-
-#calling exclude_csv function to collect all names from places.csv
-excluded_places=exclude_csv("places.csv")
-
-#gathering all names and places which shouldn't be included in password
-all_exclusions=excluded_names.union(excluded_places)
-
-def genPass():
-    password=generate_pass(all_exclusions)
-    return password
-
-#generating password by calling function
-# password = genPass()
+# password=generate_pass()
 # print(password)
