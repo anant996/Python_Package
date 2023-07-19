dc7132f4272f5f5c709d0dc8ff0650d08c04f516
Anant Chaudhary <139758290+anant996@users.noreply.github.com>
2023-07-19 11:25:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 import setuptools
 
 setuptools.setup(
-    name="pkgAnant",                     # This is the name of the package
-    version="1.1.1",                        # The initial release version
-    author="Anant Chaudhary",                     # Full name of the author
+    name="pkgAnant",
+    version="1.1.1",
+    author="Anant Chaudhary",
     description="password generator package",
+    long_description="A package for generating passwords.",
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    packages=setuptools.find_packages(),
+    package_data={'pkgAnant': ['data/*.csv']},  # Correct the package_data key
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["quicksample"],             # Name of the python package
-    install_requires=[]                     # Install other dependencies if any
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # Add any dependencies here
+    ]
 )
+
