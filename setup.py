54ff373074a55f461df0b705422d5603e4c0fa14
Anant Chaudhary <139758290+anant996@users.noreply.github.com>
2023-07-31 11:33:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 import setuptools
+import os
+
+# Get the version from the environment variable (RELEASE_VERSION) set by the GitHub Action workflow
+version = os.getenv("RELEASE_VERSION", "3.5.5")  # Provide a default version if RELEASE_VERSION is not set
 
 setuptools.setup(
-    name="pkgAnant",                     # This is the name of the package
-    version="3.5.5",                        # The initial release version
-    author="Anant Chaudhary",                     # Full name of the author
+    name="pkgAnant",
+    version=version,
+    author="Anant Chaudhary",
     description="password generator package",
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["quicksample"],             # Name of the python package
-    install_requires=[]                     # Install other dependencies if any
+    ],
+    python_requires='>=3.6',
+    py_modules=["quicksample"],
+    install_requires=[
+        # Add any dependencies your package requires here
+        # For example:
+        # "numpy",
+        # "requests",
+    ]
 )
